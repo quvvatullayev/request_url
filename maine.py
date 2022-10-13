3a7a5ae63ebd956cb4093af57b0307252d8ce2dd
tajriba <quvvatullayev@gmail.com>
2022-10-13 06:18:27 +0300

    fix

--- maine.py
+++ maine.py
@@ -8,13 +8,13 @@ def add():
     db.default_table_name = 'Vivo'
     for i in db.all():
         dict_p = {}
-        dict_p['name'] = i.get('name')
-        dict_p["company"] = i.get("company"),
-        dict_p["color"] =  i.get("color"),
-        dict_p["RAM"] =i.get("RAM"),
-        dict_p["memory"] = i.get("memory"),
-        dict_p["price"] =i.get("price"),
-        dict_p["img_url"] =i.get("img_url")
+        dict_p['name'] = i.get('name', '')
+        dict_p["company"] = i.get("company", ''),
+        dict_p["color"] =  i.get("color", ''),
+        dict_p["RAM"] =i.get("RAM", ''),
+        dict_p["memory"] = i.get("memory", ''),
+        dict_p["price"] =i.get("price", ''),
+        dict_p["img_url"] =i.get("img_url", '')
         r = requests.post(url, data=dict_p)
     return db.all()
 add()
